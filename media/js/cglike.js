85e9b4e600712feedf585194124a32dce6e236b1
pmleconte <pascal.leconte@conseilgouz.com>
2023-06-21 13:11:34 +0200

    2.0.3
    JS error if no button defined on current page
    Invalid ajax url if no menu defined for current article

--- media/js/cglike.js
+++ media/js/cglike.js
@@ -2,42 +2,45 @@
  * @module		CG Like for Joomla 4.x
  * @author		ConseilGouz
  * @license		GNU General Public License version 2 or later
+ * @version 	2.0.3
  */ 
 document.addEventListener('DOMContentLoaded', function() {
 	buttons = document.querySelectorAll('[class^="cg_like_btn_"]');
-	['click', 'touchstart'].forEach(type => {
-		buttons[0].addEventListener(type, function(e) {
-			$this = this;
-			e.stopPropagation();
-			e.preventDefault();		
-			$b = $this.getAttribute('data');
-			if ($this.disabled) {
-				return false;
-			}
-			$this.setAttribute('disabled', '')
-			url = 'index.php?option=com_ajax&plugin=cglike&action=update&id='+ $b+'&format=raw';
-			Joomla.request({
-				method   : 'POST',
-				url   : url,
-				onSuccess: function (data, xhr) {
-					$this.removeAttribute('disabled'); 
-					var parsed = JSON.parse(data);
-					if (parsed.ret == 0) {
-						icon = document.querySelector('#cg_like_icon_'+$b)
-						icon.classList.remove('icon-heart-empty');
-						icon.classList.add('icon-heart');
-						val = document.querySelector('#cg_like_val_'+$b);
-						val.innerHTML = parsed.cnt;
-						letexte = document.querySelector('#cg_result_'+$b);
-						letexte.innerHTML = parsed.msg;
-					} else {
-						letexte = document.querySelector('#cg_result_'+$b);
-						letexte.innerHTML = parsed.msg;
-					}
+	for (var i=0; i< buttons.length;i++) {
+		['click', 'touchstart'].forEach(type => {
+			buttons[i].addEventListener(type, function(e) {
+				$this = this;
+				e.stopPropagation();
+				e.preventDefault();		
+				$b = $this.getAttribute('data');
+				if ($this.disabled) {
+					return false;
 				}
+				$this.setAttribute('disabled', '')
+				url = '?option=com_ajax&plugin=cglike&action=update&id='+ $b+'&format=raw';
+				Joomla.request({
+					method   : 'POST',
+					url   : url,
+					onSuccess: function (data, xhr) {
+						$this.removeAttribute('disabled'); 
+						var parsed = JSON.parse(data);
+						if (parsed.ret == 0) {
+							icon = document.querySelector('#cg_like_icon_'+$b)
+							icon.classList.remove('icon-heart-empty');
+							icon.classList.add('icon-heart');
+							val = document.querySelector('#cg_like_val_'+$b);
+							val.innerHTML = parsed.cnt;
+							letexte = document.querySelector('#cg_result_'+$b);
+							letexte.innerHTML = parsed.msg;
+						} else {
+							letexte = document.querySelector('#cg_result_'+$b);
+							letexte.innerHTML = parsed.msg;
+						}
+					}
+				});
+				return false;
 			});
-			return false;
-		});
-	});	
+		});	
+	}			
 })
 
